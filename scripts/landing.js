2f2fc4c6ce85f3fb77a8e14d54f18a5a8df34600
SWLivingstone <scottwlivingstone@gmail.com>
2017-04-27 14:36:40 -0700

    checkpoint8-assignment.  Refactor javascript.  Add feature to landing page that runs script on click.

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,26 @@
+var points = document.getElementsByClassName('point');
 
-var animatePoints = function() {
+var revealpoint = function (arrayOfPoints) {
+for (var point = 0; point < arrayOfPoints.length; point++) {
+  arrayOfPoints[point].style.opacity = 1;
+  arrayOfPoints[point].style.transform = "scaleX(1) translateY(0)";
+  arrayOfPoints[point].style.msTransform = "scaleX(1) translateY(0)";
+  arrayOfPoints[point].style.WebkitTransform = "scaleX(1) translateY(0)";
+}
+};
 
-    var points = document.getElementsByClassName('point');
 
-    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+// I orginially wrote this code with a for (var point in arrayOfPoints) loop.
+// However, when I ran the the program I would get an error at the end
+// (the animation still functioned as desired) because the point variable
+// in the for loop would return the string 'length' during the last cycle of
+// the loop.  I was able to eleminate the error by inserting a conditional
+// statement to check if the point varriable was a number /^\d+$/.test(point).
+// I converted to a more traditional javascript for loop to make the code
+// cleaner.  Why did my first for loop return the string "length"?  I prefer
+// the sytanx of my fisrt for loop for its readability.
 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
 
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
-
-    revealFirstPoint();
-    revealSecondPoint();
-    revealThirdPoint();
-
-    };
-
-// animatePoints();
+//I just watched the solution video to compare my code to it.  Is the code
+// in the solution video better than mine?  What is the value of calling a
+// for loop outside of the function?
