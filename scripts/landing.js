b0614b11098af57072e2ec186d6b43b59ee62970
SWLivingstone <scottwlivingstone@gmail.com>
2017-04-27 16:01:16 -0700

    checkpoint 9. Add animate on scroll.

--- scripts/landing.js
+++ scripts/landing.js
@@ -12,3 +12,17 @@ var animatePoints = function() {
   };
   revealPoint(points);
 };
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints();
+}
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener("scroll", function(event) {
+    if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        animatePoints();
+    }
+  });
+};
